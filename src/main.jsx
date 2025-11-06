95a21b483d4792f914ad6f6a7aeec1fbc88d16b9
homow <homowsh897@gmail.com>
2025-11-06 19:30:47 +0330

    config BASE_PATH to main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,14 @@
-import React, {StrictMode} from 'react'
-import {createRoot} from 'react-dom/client'
-import {BrowserRouter} from 'react-router-dom'
-import "@/styles/global.css"
-import App from './App'
+import React, {StrictMode} from 'react';
+import {createRoot} from 'react-dom/client';
+import {BrowserRouter} from 'react-router-dom';
+import "@/styles/global.css";
+import App from './App';
+import BASE_PATH from "@/config.js";
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
-        <BrowserRouter>
+        <BrowserRouter basename={BASE_PATH}>
             <App/>
         </BrowserRouter>
     </StrictMode>,
-)
+);
