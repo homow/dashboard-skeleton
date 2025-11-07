b0714c5964523e29fce9aaebf70efdbbd1b5af3b
homow <homowsh897@gmail.com>
2025-11-07 19:31:41 +0330

    Change components to ltr

--- src/components/ui/Button.jsx
+++ src/components/ui/Button.jsx
@@ -1,6 +1,6 @@
 import {cn} from "@utils/ui-utils.js";
 
-export default function Button({text = "ارسال", onClick, type = "button", className = "", disabled = false, ...props}) {
+export default function Button({text = "Click", onClick, type = "button", className = "", disabled = false, ...props}) {
     return (
         <button
             type={type}
