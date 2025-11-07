0fb439287e6d1e5f042b52aa7930d24bbabc7753
homow <homowsh897@gmail.com>
2025-11-07 15:58:45 +0330

    update 404 page

--- src/pages/NotFound/index.jsx
+++ src/pages/NotFound/index.jsx
@@ -12,21 +12,20 @@ export default function NotFound() {
         <section className="h-full flex flex-col items-center justify-center gap-6 px-4 text-center">
             <h1 className="text-6xl font-extrabold text-rose-500">404</h1>
             <p className="text-lg text-gray-700 dark:text-gray-300">
-                صفحه‌ای که دنبالشی وجود نداره.
+                The page you're looking for doesn’t exist
             </p>
 
             <div className="flex gap-4 mt-4 flex-wrap justify-center">
                 {hasBackPath > 1 && (
                     <Button
-                        text="بازگشت به صفحه قبل"
+                        text="Back to Last Page"
                         onClick={goBack}
-                        className="px-6 py-2 bg-violet-500 hover:bg-violet-600 text-white rounded-lg transition-colors"
                     />
                 )}
                 <Button
-                    text="بازگشت به خانه"
+                    text="Back to Home"
                     onClick={goHome}
-                    className="px-6 py-2 bg-rose-500 hover:bg-rose-600 text-white rounded-lg transition-colors"
+                    className="bg-rose-500 hover:bg-rose-600"
                 />
             </div>
         </section>
