7a642f7419becab72cd66f767a2725a549351fd6
homow <homowsh897@gmail.com>
2025-11-07 17:18:23 +0330

    edit Loading.jsx

--- src/components/ui/Loading.jsx
+++ src/components/ui/Loading.jsx
@@ -1,4 +1,4 @@
-export default function Loading({msg = "لطفاً صبر کنید..."}) {
+export default function Loading({msg = "Please wait..."}) {
     return (
         <div
             className="fixed size-full inset-0 z-50 flex items-center justify-center bg-main-bg/60 dark:bg-main-bg/90 backdrop-blur-sm"
