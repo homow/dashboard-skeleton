9aea4b4e18b5c79562f01fffa973d2957bdd9b48
homow <homowsh897@gmail.com>
2025-11-17 02:09:31 +0330

    refactor AppRoutes.jsx

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,20 +1,9 @@
 import {lazy} from "react";
 import {createBrowserRouter} from "react-router-dom";
-import SuspenseBoundary from "@components/ui/SuspenseBoundary";
 import MainLayout from "@/layout/MainLayout";
 import PrivateRoutes from "./PrivateRoutes";
 import PublicRoutes from "./PublicRoutes";
 import App from "@/App"
-
-const lazyWithSuspense = (importFunc, className) => {
-    const Component = lazy(importFunc);
-
-    return props => (
-        <SuspenseBoundary className={className}>
-            <Component {...props}/>
-        </SuspenseBoundary>
-    );
-};
 
 const Login = lazyWithSuspense(() => import("@pages/Login"), "fixed inset-0");
 const Home = lazyWithSuspense(() => import("@pages/Home"));
